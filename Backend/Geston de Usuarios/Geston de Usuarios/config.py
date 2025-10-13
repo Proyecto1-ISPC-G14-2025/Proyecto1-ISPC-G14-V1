bb0338eed08e946fdd267caad5e95a314496c489
Juancen <juancen.361@gmail.com>
2025-10-13 17:45:46 -0300

    subida de documento IEEE830 actualizado final - juance

--- Backend/Geston de Usuarios/Geston de Usuarios/config.py
+++ Backend/Geston de Usuarios/Geston de Usuarios/config.py
@@ -1,6 +1,7 @@
 #config.py
 #Configuración para conexión MySQL
 MYSQL_HOST = "localhost"
+MYSQL_PORT = 3306         # 👈 AGREGALO AQUÍ
 MYSQL_USER = "root"
 MYSQL_PASSWORD = ""
 MYSQL_DB = "farmastock"
